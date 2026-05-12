62cce9a1201837fea4ce8c3b14a666d244879e65
franbarona <franbarona@gmail.com>
2026-05-12 16:37:32 +0200

    chore: initial project setup from Spring Initializr

---
+++ src/test/java/com/franbarona/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franbarona.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
